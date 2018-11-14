98a3b9e2321ad8c94fb649ee7597ce93123a370b
Victoria Murray <victoria.rose.murray@gmail.com>
2018-11-14 15:00:29 -0800

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -2,19 +2,32 @@
 const express = require('express')
 const exphbs = require('express-handlebars');
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 
 
 const app = express()
 
+// CONNECT TO MONGOOSE
+mongoose.connect('mongodb://localhost/contractor');
 
 // MIDDLEWARE
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+const Project = mongoose.model('Project', {
+  title: String,
+  date: String
+});
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+    Project.find()
+        .then(reviews => {
+            res.render('projects-index', { reviews: reviews });
+        })
+        .catch(err => {
+            console.log(err);
+        })
 })
 
 
